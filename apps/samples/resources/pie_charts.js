78377e7903e883cd8d2ef6af236e43a7d444fbbd
Razvan Jurca <razvan.jurca@threepillarglobal.com>
2011-01-07 10:53:36 +0200

    Added value label attributes to piechart.

--- apps/samples/resources/pie_charts.js
+++ apps/samples/resources/pie_charts.js
@@ -6,46 +6,61 @@
 // This page describes the main user interface for your application.  
 Samples.pieChartsPage = SC.Page.design({
   
-  mainView: SC.ScrollView.design({
-    contentView: SC.View.design({
-      layout: { left: 0, right: 0, top: 0, bottom: 0 },
-      classNames: ['piechart-sample'],
-      childViews: 'legend'.w(),
-      
-      legend: Sai.PieChart2View.design({
-        layout: { left: 10, top: 10, right: 10, bottom: 10 },
-        backgroundColor: 'white',
-        chartLayout: { left: 300, right: 20, top: 20, bottom: 20 },
-        data: [10, 1, 20],
-        attrs: {
-          colors: ['red', 'green', 'blue'],
-          strokeWidth: 3
-        },
-        legend: {
-          labels: ['Test A', 'Test B'],
-          layout: {
-            valign: 'center',
-            halign: 'center',
-            position: 'left',
-            height: 80,
-            width: 100
-          },
+  mainView:SC.View.design({
+    layout: { left: 0, right: 0, top: 0, bottom: 0 },
+    classNames: ['piechart-sample'],
+    childViews: 'legend'.w(),
+    
+    legend: Sai.PieChart2View.design({
+      layout: { left: 10, top: 10, right: 10, bottom: 10 },
+      backgroundColor: 'white',
+      chartLayout: { left: 300, right: 20, top: 20, bottom: 20 },
+      data: [1000, 300, 2000],
+      attrs: {
+        colors: ['red', 'green', 'blue'],
+        strokeWidth: 3,
+        clicks: YES,
+        values: {
+          percents: YES,
           text: {
-            fontSize: 16,
-            fill: 'black'
+            fontSize: 14,
+            fontWeight: 'bold'
           },
-          sample: {
-            shape: 'custom',
-            size: {width: 20, height: 14}
-          }
-        },
-        legendShapeRenderer_custom: function(canvas, position, layout, attrs) {
-          var x = position.cx + layout.dx, y = position.cy + layout.dy, w = layout.w, h = layout.h,
-              path = 'M%@,%@L%@,%@M%@,%@L%@,%@'.fmt(x,y,x+w,y+h,x+w,y,x,y+h);
-          canvas.path(path, { stroke: 'red', strokeWidth: 3 });
+          fill: 'white',
+          stroke: 'black',
+          strokeWidth: 1
         }
-      })
+      },
       
+      sliceClicked: function(index) {
+        alert("Clicked on slice #%@!".fmt(index));
+      },
+      
+      legend: {
+        labels: ['Test A', 'Test B'],
+        layout: {
+          valign: 'center',
+          halign: 'center',
+          position: 'left',
+          height: 80,
+          width: 100
+        },
+        text: {
+          fontSize: 16,
+          fill: 'black',
+          fontWeight: 'blod'
+        },
+        sample: {
+          shape: 'custom',
+          size: {width: 20, height: 14}
+        }
+      },
+      
+      legendShapeRenderer_custom: function(canvas, position, layout, attrs) {
+        var x = position.cx + layout.dx, y = position.cy + layout.dy, w = layout.w, h = layout.h,
+            path = 'M%@,%@L%@,%@M%@,%@L%@,%@'.fmt(x,y,x+w,y+h,x+w,y,x,y+h);
+        canvas.path(path, { stroke: 'red', strokeWidth: 3 });
+      }
     })
   })
 });
